126a6c8a65efac3e4883e733c9e8b91a87762490
JeanGorby <calicdanjeangorby@gmail.com>
2021-02-14 20:00:11 -0800

    state machine fix

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -55,17 +55,19 @@ void loop()
   if(state == 0) {
     if(digitalRead(buttonOne)) {
       timeLeft += 1;
+      analogWrite(ledPin, 
       delay(1000);
     }
     
-    if(digitalRead(buttonTwo)) {
+    if(digitalRead(buttonTwo) && timeLeft >= 1) {
       timeLeft -= 1;
       delay(1000);
     }
     
     if(digitalRead(buttonThree)) {
-      state = 1;
-      timeStarted = millis();
+      while(lightLevel > 1){}
+        state = 1;
+        timeStarted = millis();
     }   
   }
   else if(state == 1) {
@@ -94,7 +96,7 @@ void loop()
   else if(state == 3) {
     /* code for making speakers play nice sounds*/
     Serial.println("nice music");
-    playMelody(good_melody[]);
+    playMelody(good_melody);
     state = 0;
     timeLeft = 0;
 
@@ -130,40 +132,40 @@ void playMelody(int melody[]){
   
 }
 
-void autoTune()
-{
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-
-    // to calculate the note duration, take one second divided by the note type.
-
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-
-    int noteDuration = 1000 / noteDurations[thisNote];
-
-    if (lightLevel > low)
-  //so far, I added this conditon so that diode turns on and speaker turns on, I added this if conditon within the while loop of the speaker
-  //to hopefully excecute the sound part only when the if condition is satisfied
-    {
-      high = lightLevel;
-  
-    lightLevel = map(lightLevel, low+30, high-30, 0, 255);
-    lightLevel = constrain(lightLevel, 0, 255);
-      tone(8, melody[thisNote], noteDuration);
-  
-      // to distinguish the notes, set a minimum time between them.
-  
-      // the note's duration + 30% seems to work well:
-  
-      int pauseBetweenNotes = noteDuration * 1.30;
-  
-      delay(pauseBetweenNotes);
-  
-      // stop the tone playing:
-  
-      noTone(8);
-    }
-  }
-}  
+//void autoTune()
+//{
+//  for (int thisNote = 0; thisNote < 8; thisNote++) {
+//
+//    // to calculate the note duration, take one second divided by the note type.
+//
+//    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+//
+//    int noteDuration = 1000 / noteDurations[thisNote];
+//
+//    if (lightLevel > low)
+//  //so far, I added this conditon so that diode turns on and speaker turns on, I added this if conditon within the while loop of the speaker
+//  //to hopefully excecute the sound part only when the if condition is satisfied
+//    {
+//      high = lightLevel;
+//  
+//    lightLevel = map(lightLevel, low+30, high-30, 0, 255);
+//    lightLevel = constrain(lightLevel, 0, 255);
+//      tone(8, melody[thisNote], noteDuration);
+//  
+//      // to distinguish the notes, set a minimum time between them.
+//  
+//      // the note's duration + 30% seems to work well:
+//  
+//      int pauseBetweenNotes = noteDuration * 1.30;
+//  
+//      delay(pauseBetweenNotes);
+//  
+//      // stop the tone playing:
+//  
+//      noTone(8);
+//    }
+//  }
+//}  
 
   //manualTune();  // manually change the range from light to dark
 
